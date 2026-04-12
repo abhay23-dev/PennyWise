416b7bb6bdd13f71395c79b9d23cfdba677bf65e
Abhay Pal <2k23.cs2313908@gmail.com>
2026-04-12 15:20:56 +0530

    Hook up Signup form in singup page

--- client/src/pages/SignupPage.tsx
+++ client/src/pages/SignupPage.tsx
@@ -1,7 +1,9 @@
+import SignupForm from "@/components/Auth/SignupForm";
+
 export default function SignupPage() {
   return (
-    <main>
-      <h1>Signup Page</h1>
+    <main className="bg-slate-950 px-4 py-8 sm:px-8 sm:py-12">
+      <SignupForm />
     </main>
   )
 }
