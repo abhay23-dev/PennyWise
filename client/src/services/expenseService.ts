c6d2f7d4fe64c711ece37a38df09cbf9a4c59536
Abhay Pal <2k23.cs2313908@gmail.com>
2026-04-13 21:10:47 +0530

    create service for expense

---
+++ client/src/services/expenseService.ts
@@ -0,0 +1,14 @@
+import { ApiResponse, Expense } from "@/types"
+import api from "./api";
+
+export const createExpense = async (data: {
+  amount: number,
+  description: string,
+  category: string,
+  date: string,
+}) => {
+  const response = await api.post<ApiResponse<Expense>>("/expense", data);
+
+  return response.data;
+
+}
