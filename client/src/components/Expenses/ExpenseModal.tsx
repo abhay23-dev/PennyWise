7ccc3a9b0c79c5f405c6b2c686d2aba60881f1ae
Abhay Pal <2k23.cs2313908@gmail.com>
2026-04-14 20:22:40 +0530

    add tailwind css for expense modal

--- client/src/components/Expenses/ExpenseModal.tsx
+++ client/src/components/Expenses/ExpenseModal.tsx
@@ -10,7 +10,7 @@ export default function ExpenseModal({isOpen, onClose}: ExpenseModalProps) {
 
   function handleBackdropClick(e: React.MouseEvent<HTMLDivElement>) {
     if(!isOpen) return null;
-    
+
     if(e.target === e.currentTarget){
       onClose();
     }
@@ -19,10 +19,10 @@ export default function ExpenseModal({isOpen, onClose}: ExpenseModalProps) {
   return (
     <div onClick={handleBackdropClick}
     className="fixed inset-0 bg-black/60 flex items-center justify-center z-50 p-4">
-      <div>
-        <div>
-          <h2>Add new expense</h2>
-          <button><X className="size-5" /></button>
+      <div className="bg-slate-900 rounded-sm border border-slate-800 w-full max-w-2xl max-h-[90vh] overflow-y-auto">
+        <div className="flex items-center justify-between p-6 border-b border-slate-800">
+          <h2 className="text-2xl font-bold text-gray-100">Add new expense</h2>
+          <button onClick={onClose} className="p-2 text-gray-400 hover:text-gray-100 transition-colors rounded-sm hover:bg-slate-800"><X className="size-5" /></button>
         </div>
         <ExpenseForm onSuccess={onClose} />
       </div>
--- client/src/pages/ExpensesPage.tsx
+++ client/src/pages/ExpensesPage.tsx
@@ -1,25 +1,21 @@
-
 import ExpenseModal from "@/components/Expenses/ExpenseModal";
 import { useExpenseStore } from "@/store/expenseStore";
 import { Plus } from "lucide-react";
 import { useState } from "react";
 
 export default function ExpensesPage() {
-
   const { error, isLoading } = useExpenseStore();
 
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-
-
   return (
     <main>
-      <div>
+      <div className="border-b border-purple-950 pb-4 mb-4 flex items-center justify-between">
         <div>
-          <h1>Expenses</h1>
-          <p>Manage and track your expenses</p>
+          <h1 className="text-3xl font-bold text-gray-100">Expenses</h1>
+          <p className="text-gray-400 mt-1">Manage and track your expenses</p>
         </div>
-        <button>
+        <button className="flex items-center gap-2 px-6 py-3 bg-purple-950 text-gray-100 rounded-sm hover:bg-purple-800 transition font-medium">
           <Plus className="size-5" />
           Add Expense
         </button>
@@ -38,7 +34,10 @@ export default function ExpensesPage() {
         </div>
       )}
 
-      <ExpenseModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
+      <ExpenseModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+      />
     </main>
   );
 }
