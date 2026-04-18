8346b0cba247a440c1bb65bdca03c20fcfd3b318
Abhay Pal <2k23.cs2313908@gmail.com>
2026-04-19 02:09:37 +0530

    create store for analytics route

---
+++ client/src/store/analyticsStore.ts
@@ -0,0 +1,117 @@
+import { AnalyticsState } from "@/types/analytics.types";
+import { create } from "zustand";
+import { getDashboardStats as getDashboardStatsService, getMonthlyTotals as getMonthlyTotalsService, getCategoryStats as getCategoryStatsService, getTrends as getTrendsService } from "@/services/analyticsService";
+import { AxiosError } from "axios";
+
+interface AnalyticsStore extends AnalyticsState {
+  getDashboardStats: () => Promise<void>;
+  getMonthlyTotals: () => Promise<void>;
+  getCategoryStats: () => Promise<void>;
+  getTrends: () => Promise<void>;
+  clearError: () => void;
+}
+
+export const useAnalyticsStore = create<AnalyticsStore>((set) => ({
+  categoryData: [],
+  monthlyData: [],
+  dashboardStats: null,
+  trends: [],
+  isLoading: false,
+  error: null,
+
+  getDashboardStats: async () => {
+    set({
+      isLoading: true,
+      error: null,
+    })
+
+    try {
+      const response = await getDashboardStatsService();
+      if(response.data) {
+        set({
+          dashboardStats: response.data,
+          isLoading: false,
+          error: null
+        });
+      }
+    } catch(error) {
+      const err = error as AxiosError<{error: string}>;
+      set({
+        error: err.response?.data?.error,
+        isLoading: false
+      })
+    }
+  },
+  getCategoryStats: async () => {
+    set({
+      isLoading: true,
+      error: null,
+    })
+
+    try {
+      const response = await getCategoryStatsService();
+      if(response.data) {
+        set({
+          categoryData: response.data,
+          isLoading: false,
+          error: null
+        });
+      }
+    } catch(error) {
+      const err = error as AxiosError<{error: string}>;
+      set({
+        error: err.response?.data?.error,
+        isLoading: false
+      })
+    }
+  },
+  getMonthlyTotals: async () => {
+    set({
+      isLoading: true,
+      error: null,
+    })
+
+    try {
+      const response = await getMonthlyTotalsService();
+      if(response.data) {
+        set({
+          monthlyData: response.data,
+          isLoading: false,
+          error: null
+        });
+      }
+    } catch(error) {
+      const err = error as AxiosError<{error: string}>;
+      set({
+        error: err.response?.data?.error,
+        isLoading: false
+      })
+    }
+  },
+  getTrends: async () => {
+    set({
+      isLoading: true,
+      error: null,
+    })
+
+    try {
+      const response = await getTrendsService();
+      if(response.data) {
+        set({
+          trends: response.data,
+          isLoading: false,
+          error: null
+        });
+      }
+    } catch(error) {
+      const err = error as AxiosError<{error: string}>;
+      set({
+        error: err.response?.data?.error,
+        isLoading: false
+      })
+    }
+  },
+  clearError: () => {
+    set({error: null});
+  }
+}))
