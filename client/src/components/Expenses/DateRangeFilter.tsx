4b85f858af5cf86f7a0922ff2946a48fef5b3cb5
Abhay Pal <2k23.cs2313908@gmail.com>
2026-04-18 10:33:12 +0530

    create the component for DateRangeFilter

---
+++ client/src/components/Expenses/DateRangeFilter.tsx
@@ -0,0 +1,43 @@
+import { useExpenseStore } from "@/store/expenseStore";
+
+export default function DateRangeFilter() {
+  const { filters, setDateRange } = useExpenseStore();
+
+  function handleStartDateChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const newStart = e.target.value || null;
+
+    setDateRange(newStart, filters.endDate ?? null);
+  }
+
+  function handleEndDateChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const newEnd = e.target.value || null;
+
+    setDateRange(filters.startDate ?? null, newEnd);
+  }
+
+  return (
+    <div>
+      <div>
+        <label htmlFor="start-date">Start Date</label>
+        <input
+          type="date"
+          id="start-date"
+          onChange={handleStartDateChange}
+          value={filters.startDate || ""}
+          min={filters.startDate || undefined}
+          
+        />
+      </div>
+      <div>
+        <label htmlFor="end-date">Start Date</label>
+        <input
+          type="date"
+          id="end-date"
+          onChange={handleEndDateChange}
+          value={filters.startDate || ""}
+          max={filters.endDate || undefined}
+        />
+      </div>
+    </div>
+  );
+}
