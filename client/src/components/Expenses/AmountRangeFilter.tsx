b9d78b27445a3c1e6b5ee7567e6c355f488779c5
Abhay Pal <2k23.cs2313908@gmail.com>
2026-04-18 12:01:05 +0530

    add css to AmountRangeFilter component

--- client/src/components/Expenses/AmountRangeFilter.tsx
+++ client/src/components/Expenses/AmountRangeFilter.tsx
@@ -16,9 +16,9 @@ export default function DateRangeFilter() {
   }
 
   return (
-    <div>
-      <div>
-        <label htmlFor="min-amount">Min Amount ($)</label>
+    <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+      <div className="flex flex-col gap-2">
+        <label htmlFor="min-amount" className="text-sm font-medium text-gray-400">Min Amount ($)</label>
         <input
           type="number"
           id="min-amount"
@@ -27,10 +27,11 @@ export default function DateRangeFilter() {
           min="0"
           step="0.01"
           placeholder="0.00"
+          className="px-4 py-2.5 bg-purple-950 rounded-sm text-gray-100 border-none outline-none"
         />
       </div>
-      <div>
-        <label htmlFor="max-amount">Max Amount ($)</label>
+      <div className="flex flex-col gap-2">
+        <label htmlFor="max-amount" className="text-sm font-medium text-gray-400">Max Amount ($)</label>
         <input
           type="number"
           id="max-amount"
@@ -39,6 +40,7 @@ export default function DateRangeFilter() {
           step="0.01"
           placeholder="No limit"
           min={filters.minAmount || "0"}
+          className="px-4 py-2.5 bg-purple-950 rounded-sm text-gray-100 border-none outline-none"
         />
       </div>
     </div>
