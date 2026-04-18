cbf1eb4351544bafb0d7f22085e3c7de2446ccf2
Abhay Pal <2k23.cs2313908@gmail.com>
2026-04-18 12:04:37 +0530

    add css to DateRangeFilter component

--- client/src/components/Expenses/DateRangeFilter.tsx
+++ client/src/components/Expenses/DateRangeFilter.tsx
@@ -16,26 +16,27 @@ export default function DateRangeFilter() {
   }
 
   return (
-    <div>
-      <div>
-        <label htmlFor="start-date">Start Date</label>
+    <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+      <div className="flex flex-col gap-2">
+        <label htmlFor="start-date" className="text-sm font-medium text-gray-400">Start Date</label>
         <input
           type="date"
           id="start-date"
           onChange={handleStartDateChange}
           value={filters.startDate || ""}
           max={filters.endDate || undefined}
-          
+          className="px-4 py-2.5 bg-purple-950 rounded-sm text-gray-100 border-none outline-none"
         />
       </div>
-      <div>
-        <label htmlFor="end-date">End Date</label>
+      <div className="flex flex-col gap-2">
+        <label htmlFor="end-date" className="text-sm font-medium text-gray-400">End Date</label>
         <input
           type="date"
           id="end-date"
           onChange={handleEndDateChange}
           value={filters.endDate || ""}
           min={filters.startDate || undefined}
+          className="px-4 py-2.5 bg-purple-950 rounded-sm text-gray-100 border-none outline-none"
         />
       </div>
     </div>
